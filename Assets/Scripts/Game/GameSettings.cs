eed9e11d32c8443875060fe98a577d8a2d180aa7
Marco <marco.pdsv@gmail.com>
2016-03-31 00:58:09 -0300

    Generating fixed vertical pipes

--- Assets/Scripts/Game/GameSettings.cs
+++ Assets/Scripts/Game/GameSettings.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class GameSettings : Settings {
     public float initMainMenuDelay = 0.3f;
+    public float launchFixedVerticalPipePeriod = 2f;
 
     private static GameSettings _instance;
 
--- Assets/Scripts/Game/GameStates/LaunchingVerticalPipesGS.cs
+++ Assets/Scripts/Game/GameStates/LaunchingVerticalPipesGS.cs
@@ -4,13 +4,21 @@ using System;
 
 public class LaunchingVerticalPipesGS : GameState {
 
+    private float delta;
+
     public LaunchingVerticalPipesGS() {
     }
 
     protected override void Enter() {
+        delta = 0.0f;
     }
 
     protected override State<GameController, GameSettings, GameData> Update() {
+        delta += Time.deltaTime;
+        if( delta >= GameSettings.Instance.launchFixedVerticalPipePeriod) {
+            delta = 0.0f;
+            PipesFactory.Instance.CreateFixesVerticalPipe();
+        }
         return null;
     }
 
